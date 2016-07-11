712243079c3a8f65453504df5d206a73dd030428
stitchinspace <reninspace@gmail.com>
2016-07-10 19:32:32 -0700

    search populates, style pretty good

--- script.js
+++ script.js
@@ -6,30 +6,35 @@ document.addEventListener("DOMContentLoaded", function() { //loads page after ht
     });
 
     document.querySelector("#button").addEventListener("click", function() {
-var query= document.querySelector("#text").value;
+        var query = document.querySelector("#text").value;
 
-for (var i in query) {
-  if (i === ' ') {query[i] = "%20";}
-}  console.log("query=" + query);
-if (query == 'Search wikipedia' || query == '') {
+        for (var i in query) {
+            if (i === ' ') {
+                query[i] = "%20";
+            }
+        }
+        console.log("query=" + query);
+        if (query == 'Search wikipedia' || query == '') {
             text = "Please enter a search term";
-        } else { makeRequest(query);}
-});
+        } else {
+            makeRequest(query);
+        }
+    });
 
 
-  function makeRequest(query) {
-$.ajax({
-  type: 'GET',
-  url: 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + query + '&limit=3&namespace=0&format=json&callback=?',
-jsonp: "$jsonp",
-  dataType: "jsonp",
-}).done(function(data) {
-$("#result1").html(data[1][0]);
-$("#resulta").html(data[2][0]);
-$("#result2").html(data[1][1]);
-$("#resultb").html(data[2][1]);
-$("#result3").html(data[1][2]);
-$("#resultc").html(data[2][2]);
-});
-}
+    function makeRequest(query) {
+        $.ajax({
+            type: 'GET',
+            url: 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + query + '&limit     =3&namespace=0&format=json&callback=?',
+            jsonp: "$jsonp",
+            dataType: "jsonp",
+        }).done(function(data) {
+            $("#result1").html(data[1][0]);
+            $("#resulta").html(data[2][0]);
+            $("#result2").html(data[1][1]);
+            $("#resultb").html(data[2][1]);
+            $("#result3").html(data[1][2]);
+            $("#resultc").html(data[2][2]);
+        });
+    }
 }); //close DOMcontentLoaded function
